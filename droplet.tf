a9b0da4e87b050967ad26e6bcc554c401d3cf731
drawsmcgraw <drew.malone@gmail.com>
2020-07-21 20:32:30 -0400

    cleanup

--- droplet.tf
+++ droplet.tf
@@ -8,13 +8,13 @@ resource "digitalocean_droplet" "workshop-droplet" {
   # Copy in the bash script we want to execute.
   # The source is the location of the bash script
   # on the local linux box you are executing terraform
-  # from.  The destination is on the new AWS instance.
+  # from.  The destination is on the new VM.
   provisioner "file" {
     source      = "user_data.sh"
     destination = "/tmp/user_data.sh"
   }
 
-  # Change permissions on bash script and execute from ec2-user.
+  # Change permissions on bash script and execute
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/user_data.sh",
@@ -22,7 +22,7 @@ resource "digitalocean_droplet" "workshop-droplet" {
     ]
   }
 
-  # Login to the ec2-user with the aws key.
+  # How do we log into the VM?
   connection {
     type        = "ssh"
     user        = "root"
